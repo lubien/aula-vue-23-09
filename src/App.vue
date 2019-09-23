fc43bd3cb397543cf06ba2c883bfbdeb6290f950
João Ferreira <lubien1996@gmail.com>
2019-09-23 09:58:39 -0300

    Segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <p style="color: red">
+      Abd def ghi
+    </p>
   </div>
 </template>
 
@@ -10,8 +11,13 @@ import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
+
   components: {
     HelloWorld
+  },
+
+  beforeDestroyed() {
+    // salva rascucho
   }
 };
 </script>
@@ -21,7 +27,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
